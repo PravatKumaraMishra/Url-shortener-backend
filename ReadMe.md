21a28e9c462700288e7c41ca82920adf3a8a7c18
pravatkumarmishra <pravatkumarmishrapkm@gmail.com>
2026-01-03 18:30:44 +0530

    Structure Jwt Response & Add ReadMe

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+## Upcoming Features:
+- Custom short URL.
+- Expiration of URL if unused.
+- can be used without login
+- subDomain for redirection
--- src/main/java/me/pravat/uShort/controller/UserController.java
+++ src/main/java/me/pravat/uShort/controller/UserController.java
@@ -2,9 +2,11 @@ package me.pravat.uShort.controller;
 
 import lombok.RequiredArgsConstructor;
 import me.pravat.uShort.dto.AuthRequest;
+import me.pravat.uShort.dto.JwtResponse;
 import me.pravat.uShort.entity.User;
 import me.pravat.uShort.service.JwtService;
 import me.pravat.uShort.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -33,12 +35,13 @@ public class UserController {
     // Removed the role checks here as they are already managed in SecurityConfig
 
     @PostMapping("/login")
-    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+    public ResponseEntity<JwtResponse> authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword())
         );
         if (authentication.isAuthenticated()) {
-            return jwtService.generateToken(authRequest.getEmail());
+            var response = jwtService.generateToken(authRequest.getEmail());
+            return ResponseEntity.ok(new JwtResponse(response));
         } else {
             throw new UsernameNotFoundException("Invalid user request!");
         }
