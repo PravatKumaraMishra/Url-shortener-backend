c600057d66f8ccaca23acc0037d9dcaa8350938c
pravatkumarmishra <pravatkumarmishrapkm@gmail.com>
2026-01-28 09:49:29 +0530

    Added Swagger

--- src/main/java/me/pravat/uShort/config/SecurityConfig.java
+++ src/main/java/me/pravat/uShort/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
                         //.requestMatchers("/auth/admin/**").hasAuthority("ROLES_ADMIN")
 
                         // All other endpoints require authentication
+                        .requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
